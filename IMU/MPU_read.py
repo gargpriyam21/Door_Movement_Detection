70f240b2b7d1e2edea9d2bcefc633844ce044121
Divyag Doshi <divyangdoshi02@gmail.com>
2022-04-10 06:10:19 -0400

    updated code for doing API call and cycle detection

--- IMU/MPU_read.py
+++ IMU/MPU_read.py
@@ -3,6 +3,10 @@ from xml.sax.handler import property_declaration_handler
 import paho.mqtt.client as mqtt
 import time
 import datetime
+from copy import deepcopy
+from feature_creation import create_data
+import numpy as np
+import requests
 
 # Constants
 BROKER_IP_ADDRESS = 'localhost'
@@ -11,12 +15,15 @@ KEEPALIVE = 60
 
 TOPIC_IMU = "ncsu/iot/G11/IMU"
 
+BEST_WINDOW_SIZE = 3
+
 SUCCESS_CODE = 0
 
-IMU_SAMPLING_RATE = 0.01 # Unit: seconds
+IMU_SAMPLING_RATE = 0.01  # Unit: seconds
 
 # Variables
-mpu = mpu6050(0x68) # assign mpu to the right I2C address
+mpu = mpu6050(0x68)  # assign mpu to the right I2C address
+
 
 # Callbacks
 # Called on MQTT connect
@@ -26,6 +33,7 @@ def mqtt_connect(client, data, flags, rc):
     else:
         print(f"Failed connection. Code {rc}")
 
+
 # Called on MQTT disconnect
 def mqtt_disconnect(client, data, rc):
     if rc == SUCCESS_CODE:
@@ -33,63 +41,114 @@ def mqtt_disconnect(client, data, rc):
     else:
         print(f"Forced disconnect. Code {rc}")
 
+
 def read_MPU(sensor):
-	vals = [sensor.get_accel_data().get('x'),sensor.get_accel_data().get('y'),sensor.get_accel_data().get('z'),sensor.get_gyro_data().get('x'),sensor.get_gyro_data().get('y'),sensor.get_gyro_data().get('z')]
-	return vals
+    vals = [sensor.get_accel_data().get('x'), sensor.get_accel_data().get('y'), sensor.get_accel_data().get('z'),
+            sensor.get_gyro_data().get('x'), sensor.get_gyro_data().get('y'), sensor.get_gyro_data().get('z')]
+    return vals
+
+
+current_label = 'STATIONARY'
+past_gyro_data = [0, 0, 0]
+avg = 0
+past_labels = ['STATIONARY', 'STATIONARY', 'STATIONARY']
+api_payload = []
+cycle_start = False
+
+
+def update_label(gyro_x):
+    global current_label, past_gyro_data, avg
+
+    past_gyro_data[2] = past_gyro_data[1]
+    past_gyro_data[1] = past_gyro_data[0]
+    past_gyro_data[0] = gyro_x
+
+    avg = (past_gyro_data[0] + past_gyro_data[1] + past_gyro_data[2]) / 3
+
+    if avg > 50:
+        current_label = "OPEN"
+    elif avg < -50:
+        current_label = "CLOSED"
+    else:
+        current_label = "STATIONARY"
+
+
+def detect_cycle(data):
+    global current_label, past_labels, api_payload, cycle_start
+
+    last_removed = past_labels[2]
+    past_labels[2] = past_labels[1]
+    past_labels[1] = past_labels[0]
+    past_labels[0] = current_label
+
+    ret_flag = False
+    if cycle_start and past_labels[0] == past_labels[1] == past_labels[2] == 'STATIONARY':
+        cycle_start = False
+        ret_flag = True
+    elif not cycle_start and current_label != 'STATIONARY':
+        cycle_start = True
+        api_payload = []
+        api_payload.append(data)
+    elif cycle_start and current_label != 'STATIONARY':
+        api_payload.append(data)
+
+    return ret_flag, last_removed
+
+
+def get_prediction():
+    global api_payload
+    model_data = create_data([deepcopy(api_payload)], BEST_WINDOW_SIZE)
+    model_data = model_data.tolist()
+
+    API_KEY = "ysQ6mF886s6PwVPZt-2KqdmX4CnQZ78P3zjuEQwm4Y5v"
+    token_response = requests.post('https://iam.cloud.ibm.com/identity/token',
+                                   data={"apikey": API_KEY,
+                                         "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
+    mltoken = token_response.json()["access_token"]
+
+    # header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
+
+    payload_scoring = {"input_data": [{"fields": ["f0", "f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8"],
+                                       "values": model_data}]}
+    response_scoring = requests.post(
+        'https://us-south.ml.cloud.ibm.com/ml/v4/deployments/661e446e-4343-49f0-ba95-4a074de98a40/predictions?version=2022-04-10',
+        json=payload_scoring,
+        headers={'Authorization': 'Bearer ' + mltoken})
+    result = response_scoring.json()
+
+    prediction_value = "CLOSED" if result['predictions'][0]['values'][0][0] == 0 else "OPEN"
+    return prediction_value
+
 
 # main
 def main():
-	
-	# Create client
-	mqttClient = mqtt.Client("DoorIMU")
+    # Create client
+    mqttClient = mqtt.Client("DoorIMU")
 
-	# Define callbacks
-	mqttClient.on_connect = mqtt_connect
-	mqttClient.on_disconnect = mqtt_disconnect
+    # Define callbacks
+    mqttClient.on_connect = mqtt_connect
+    mqttClient.on_disconnect = mqtt_disconnect
 
-	# Connect to broker
-	mqttClient.connect(host = BROKER_IP_ADDRESS, port = PORT, keepalive = KEEPALIVE)
-	mqttClient.loop_start()
+    # Connect to broker
+    mqttClient.connect(host=BROKER_IP_ADDRESS, port=PORT, keepalive=KEEPALIVE)
+    mqttClient.loop_start()
 
-	#f = open("door_open.txt", 'w')
-	print("starting loop")
+    # f = open("door_open.txt", 'w')
+    print("starting loop")
 
-	#label = ", STATIONARY"
-	#pastData = [0,0,0]
-	#avg = 0
+    while True:
+        data = read_MPU(mpu)
+        # print(data)
 
-	while True:
-		data = read_MPU(mpu)
-		#print(data)
+        # REST  call
+        update_label(data[3])
+        cycle_detected, cycle_label = detect_cycle(data)
+        if cycle_detected:
+            returned_label = get_prediction()
+            mqttClient.publish(TOPIC_IMU, returned_label, 2, True)
 
-		'''
-		@Divyang
-		Make REST API call here and get response
-		'''
-		returnedLabel = "enter label response here"
-
-		mqttClient.publish(TOPIC_IMU, returnedLabel, 2, True)
-
-		# pastData[2] = pastData[1]
-		# pastData[1] = pastData[0]
-		# pastData[0] = mpu.get_gyro_data().get('x')
-
-		# avg = (pastData[0] + pastData[1] + pastData[2]) / 3
-
-		# if avg > 50:
-		# 	label = ", OPEN"
-		# elif avg < -50:
-		# 	label = ", CLOSED"
-		# else:
-		# 	label = ", STATIONARY"
-
-		# f.write(str(datetime.datetime.now()) + ": " + str(data) + label)
-		# f.write('\n')
-
-		# print(label)
-
-		time.sleep(IMU_SAMPLING_RATE)
+        time.sleep(IMU_SAMPLING_RATE)
 
 
 if __name__ == '__main__':
-	main()
+    main()
