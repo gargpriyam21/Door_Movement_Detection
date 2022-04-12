c72a9d331b3c446bec26029953c0bf2afb0599a4
Divyag Doshi <divyangdoshi02@gmail.com>
2022-04-11 21:10:26 -0400

    updated to use IBM cloud mqtt

--- Laptop/laptop.py
+++ Laptop/laptop.py
@@ -1,54 +1,71 @@
 from xml.sax.handler import property_declaration_handler
 import paho.mqtt.client as mqtt
-import RPi.GPIO as GPIO
 import time
+from datetime import datetime
+import wiotp.sdk.application
+import json
 import numpy as np
 
 # Constants
-BROKER_IP_ADDRESS = 'localhost'
-PORT = 1883
-KEEPALIVE = 60
+# BROKER_IP_ADDRESS = '192.168.1.233'
+# PORT = 1883
+# KEEPALIVE = 60
 
-TOPIC_DOORSENSOR = "ncsu/iot/G11/IMU"
+TOPIC_DOORSENSOR = "DoorStatus"
 
-SUCCESS_CODE = 0
 
-# Callbacks
-# Called on MQTT connect
-def mqtt_connect(client, data, flags, rc):
-    if rc == SUCCESS_CODE:
-        print(f"Connected")
-    else:
-        print(f"Failed connection. Code {rc}")
+# SUCCESS_CODE = 0
 
-# Called on MQTT disconnect
-def mqtt_disconnect(client, data, rc):
-    if rc == SUCCESS_CODE:
-        print("Graceful disconnect successful")
-    else:
-        print(f"Forced disconnect. Code {rc}")
+# # Callbacks
+# # Called on MQTT connect
+# def mqtt_connect(client, data, flags, rc):
+#     if rc == SUCCESS_CODE:
+#         print(f"Connected")
+#     else:
+#         print(f"Failed connection. Code {rc}")
+#
+# # Called on MQTT disconnect
+# def mqtt_disconnect(client, data, rc):
+#     if rc == SUCCESS_CODE:
+#         print("Graceful disconnect successful")
+#     else:
+#         print(f"Forced disconnect. Code {rc}")
 
 # Called on MQTT msg received
-def mqtt_message_rcv(client, data, message):
-    receivedLabel = float(message.payload.decode("utf-8"))
-    print(f"Door is {receivedLabel}")
+def mqtt_message_rcv(evt):
+    payload = json.dumps(evt.data).strip("{\" }").replace('"', '').split(":")
+    receivedLabel = payload[1].lstrip(' ')
+    TIMESTAMP = datetime.now().strftime("%d/%m/%Y %H:%M:%S %p")
+    print("{} : Door is {}".format(TIMESTAMP, receivedLabel))
+
 
 # Main
 def main():
-
     # Create client
-    mqttClient = mqtt.Client("DoorIMU")
+    try:
+        options = wiotp.sdk.application.parseConfigFile("laptop_config.yaml")
+        mqttClient = wiotp.sdk.application.ApplicationClient(options)
+        mqttClient.connect()
+        mqttClient.subscribeToDeviceEvents(typeId="IOT_Assignment_4", deviceId='3', eventId=TOPIC_DOORSENSOR)
+        mqttClient.deviceEventCallback = mqtt_message_rcv
+        while True:
+            pass
+    except Exception as e:
+        print("Exception: ", e)
 
-    # Define callbacks
-    mqttClient.on_connect = mqtt_connect
-    mqttClient.on_message = mqtt_message_rcv
-    mqttClient.on_disconnect = mqtt_disconnect
+    # mqttClient = mqtt.Client("DoorIMUlaptop")
+    #
+    # # Define callbacks
+    # mqttClient.on_connect = mqtt_connect
+    # mqttClient.on_message = mqtt_message_rcv
+    # mqttClient.on_disconnect = mqtt_disconnect
+    #
+    # # Connect to broker
+    # mqttClient.connect(host = BROKER_IP_ADDRESS, port = PORT, keepalive = KEEPALIVE)
+    # mqttClient.loop_start()
+    #
+    # mqttClient.subscribe(TOPIC_DOORSENSOR)
 
-    # Connect to broker
-    mqttClient.connect(host = BROKER_IP_ADDRESS, port = PORT, keepalive = KEEPALIVE)
-    mqttClient.loop_start()
-
-    mqttClient.subscribe(TOPIC_DOORSENSOR)
 
 if __name__ == '__main__':
     main()
